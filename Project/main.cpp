fef388fe4d597b913f192d71f6b6ec1dbb126bd6
davidpwnz <dcovell@live.com>
2017-04-05 20:09:01 -0700

    Add files via upload

--- Project/main.cpp
+++ Project/main.cpp
@@ -1,53 +1,66 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/* 
- * File:   main.cpp
- * Author: rcc
- *
- * Created on April 5, 2017, 1:56 PM
- */
-
 #include <cstdlib>
 #include <iostream>
 #include <ctime>
+
 using namespace std;
 
-/*
- * 
- */
+
 int main(int argc, char** argv) {
     int player[4];
     int comp[4];
+    bool end = false;
     srand(time(0));
     
-    for(int i = 0, inc = 1; i < 4; i++, inc++) {
-        comp[i] = rand()%4+1;
+    for(int i = 0; i < 4; i++) {
+        comp[i] = rand()%6+1;
         if(i > 0) {
-            for(int k = 0;k < inc; k++) {
-                while(comp[i] == comp[i-(k+1)]) {
-                    comp[i] = rand()%4+1;
+            for(int k = 0;k < i; k++) {
+                while(comp[i] == comp[k]) {
+                    comp[i] = rand()%6+1;
+                    k = 0;
                 }
             }
         }
     }
     
-//    for(int i = 0; i < 4; i++) {
-//        cin >> player[i];
-//    }
-//   
-//    for(int i = 0; i < 4; i++) {
-//        cout << player[i];
-//    }
-    cout << endl;
+    do {
+        int games = 0;
+        cout << "Enter numbers 1 - 6" << endl;    
+        for(int i = 0; i < 4; i++) {
+            cin >> player[i];
+        }
+
+        for(int i = 0; i < 4; i++) {
+            cout << player[i];
+        }
+
+        cout << "\n\n";
+        for(int i = 0; i < 4; i++) {
+            int inc = 0;
+            if(comp[i] == player[i]) {
+                    cout << "RED " << "Space: " << i+1 << endl;
+                    inc++;
+                    if(inc = 4)
+                        end = true;
+            }
+            else {
+                for(int k = 0; k < 4; k++) {
+                    if (comp[i] == player[k])
+                        cout << "WHITE " << "Space: " << k+1 <<endl;
+                }
+            }
+        }
+        games++;
+        if(games == 10)
+            end == true;
+    } while(end == false); 
+
+    
+    cout << "\n\n";
+    
     for(int i = 0; i < 4; i++) {
         cout << comp[i];
     }
     
-    
     return 0;
 }
-
